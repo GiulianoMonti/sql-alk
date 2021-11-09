c265a8b405be536e1ef4020ebb1490000339b0b7
giulianodanielmonti <giulianm@gmail.com>
2021-11-09 20:07:25 -0300

    fixfix

---
+++ sql4.sql
@@ -0,0 +1,21 @@
+tema 3.1
+
+a)
+CREATE TABLE persona 
+(id INT, nombre VARCHAR(45) NOT NULL);
+
+b)
+INSERT INTO persona 
+VALUES 	  (3,”juan”),
+	      (1,”jose”),
+	      (6,"charles"),
+	      (5,"maria"),
+	      (4,"estela"),
+	      (2,"juana"),
+
+c) 
+SELECT * FROM persona;
+
+d) 
+ALTER TABLE persona
+ADD CONSTRAINT PK_id PRIMARY KEY (id);
